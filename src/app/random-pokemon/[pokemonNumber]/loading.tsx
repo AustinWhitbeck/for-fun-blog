087a2559be2f19f4177dcbef9cfa3dbb3edf880c
Austin Whitbeck <AustinWhitbeckArt@gmail.com>
2023-06-04 16:16:34 -0400

    updated icon and made some small changes to globals and styling

---
+++ src/app/random-pokemon/[pokemonNumber]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <div>Your random pokemon is loading...</div>
+  );
+}
+
+export default Loading;
--- src/app/random-pokemon/[pokemonNumber]/page.tsx
+++ src/app/random-pokemon/[pokemonNumber]/page.tsx
@@ -1,6 +1,7 @@
-import { getRandomPokemonData } from '@/app/layout';
+import { getRandomPokemonData, pickRandomPokemonNumber } from '@/app/layout';
 import firstLetterUppercase from '@/helper-functions/firstLetterUppercase';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export async function generateMetadata({ params }: { params: { pokemonNumber: string } }) {
@@ -17,14 +18,35 @@ export const RandomPokemon = async ({ params }: { params: { pokemonNumber: strin
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const { back_default, front_default } = sprites;
   const pokemonName = firstLetterUppercase(name);
+  const nextRandomPokemonNumber = pickRandomPokemonNumber();
+
+  const spritesFrontRender = () => {
+    if (front_default && front_default !== null) {
+      return <Image src={front_default} alt="Sprite Front" width={200} height={200} />;
+    }
+    return '';
+  };
+
+  const spritesBackRender = () => {
+    if (back_default && back_default !== null) {
+      return <Image src={back_default} alt="Sprite Front" width={200} height={200} />;
+    }
+    return '';
+  };
+
+  const allSpritesRender = () => (
+    <>
+      {spritesBackRender()}
+      {spritesFrontRender()}
+    </>
+  );
   return (
     <div>
       <h1>
-        {`Random Pokemon: ${pokemonName}`}
+        {`Random Pokemon: ${pokemonName || 'Error'}`}
       </h1>
-      <Image src={front_default} alt="Sprite Front" width={200} height={200} />
-      <Image src={back_default} alt="Sprite Back" width={200} height={200} />
-
+      <Link href={`/random-pokemon/${nextRandomPokemonNumber}`}>Next Random Pokemon!</Link>
+      {allSpritesRender()}
     </div>
   );
 };
